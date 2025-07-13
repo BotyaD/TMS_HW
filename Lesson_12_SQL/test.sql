9b09e23ec056469b590c2bb620aa326c8a08f331
BotyaD <botyadoshanov@gmail.com>
2025-07-13 11:46:21 +0300

    Изменения порядка создания таблиц

--- Lesson_12_SQL/test.sql
+++ Lesson_12_SQL/test.sql
@@ -1,36 +1,36 @@
 create database lesson14;
 use lesson14;
+
+
 create table users(
 	id int unsigned primary key auto_increment,
     username varchar(64) unique not null,
     password varchar(64) not null,
-    email varchar(30) null
+    email varchar(30) unique null
 );
 
+create table seller(
+	id int unsigned primary key auto_increment,
+    company varchar(64) unique not null,
+    phone varchar(20) unique null
+);
+
+create table products(
+	id int unsigned primary key auto_increment,
+    name varchar(25) not null,
+    cost int not null,
+    count int not null,
+    seller_id int unsigned not null,
+
+    foreign  key (seller_id) references seller (id)
+);
 
 create table orders(
 	id int unsigned primary key auto_increment,
     user_id int unsigned not null,
     product_id int unsigned not null,
-    count int unsigned,
+    count int unsigned not null,
 
     foreign key (user_id) references users (id),
-    foreign key (product_id) references users (id)
-);
-
-create table products(
-	id int unsigned primary key auto_increment,
-    name varchar(25) unique not null,
-    cost int not null,
-    count int,
-    seller_id int unsigned,
-
-    foreign  key (seller_id) references seller (id)
-);
-
-
-create table seller(
-	id int unsigned primary key auto_increment,
-    company varchar(64) unique not null,
-    phone varchar(20) null
+	foreign key (product_id) references products (id)
 );
